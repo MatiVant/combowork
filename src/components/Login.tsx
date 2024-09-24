523f06927577e5889d444ae5ab39822620b9bcde
Matías Trovant <mtrovant@gmail.com>
2024-09-24 15:59:53 -0300

    forzado de minusculas

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,34 +1,36 @@
-import React, { useContext, useState } from "react";
-import AuthService from "../services/AuthService";
-import { ErrorResponse, Link } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { User } from "../types/user";
-import * as yup from "yup";
-import context from "../context";
-import ForgotPassword from "./ForgotPassword";
-import CircularProgress from "@mui/material/CircularProgress";
-import shoppingCartService from "../services/shoppingCartService";
-import { toast } from "react-toastify";
+import React, { useContext, useState } from 'react';
+import AuthService from '../services/AuthService';
+import { ErrorResponse, Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { User } from '../types/user';
+import * as yup from 'yup';
+import context from '../context';
+import ForgotPassword from './ForgotPassword';
+import CircularProgress from '@mui/material/CircularProgress';
+import shoppingCartService from '../services/shoppingCartService';
+import { toast } from 'react-toastify';
 //
 const user = yup
   .object({
     mail: yup
       .string()
-      .email("El mail es invalido")
-      .required("El mail es requerido"),
+      .email('El mail es invalido')
+      .required('El mail es requerido'),
     password: yup
       .string()
-      .required("Su contraseña no tiene la cantidad de caracteres correspondientes")
+      .required(
+        'Su contraseña no tiene la cantidad de caracteres correspondientes',
+      )
       .test(
-        "password-validation",
-        "Ingrese email y contraseña",
+        'password-validation',
+        'Ingrese email y contraseña',
         (value: any) => {
           if (!value) {
             return false;
           }
           return value && value.length >= 8;
-        }
+        },
       ),
   })
   .required();
@@ -36,10 +38,11 @@ const user = yup
 type FormData = yup.InferType<typeof user>;
 //////
 const Login = () => {
-  const [email, setEmail] = useState<string>("");
-  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [email, setEmail] = useState<string>('');
+  const [errorMessage, setErrorMessage] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
-  const [isUserForgotPass, setIsUserForgotPass] = useState<boolean>(false);
+  const [isUserForgotPass, setIsUserForgotPass] =
+    useState<boolean>(false);
   const userLogged = useContext(context);
   const {
     register,
@@ -53,64 +56,68 @@ const Login = () => {
   const getUser = async () => {
     const response = await AuthService.findUser();
     if (response.data === null) {
-      setErrorMessage("Email o contraseña incorrectos");
+      setErrorMessage('Email o contraseña incorrectos');
     }
-    if (response.data.state === "inactive") {
-      setErrorMessage("El usuario se encuentra Inactivo");
+    if (response.data.state === 'inactive') {
+      setErrorMessage('El usuario se encuentra Inactivo');
     }
-    localStorage.setItem("currentUser", JSON.stringify(response.data));
-    const storedCurrentUser = localStorage.getItem("currentUser");
-    const initialCurrentUser: User | undefined = storedCurrentUser
-      ? JSON.parse(storedCurrentUser)
-      : undefined;
+    localStorage.setItem(
+      'currentUser',
+      JSON.stringify(response.data),
+    );
+    const storedCurrentUser =
+      localStorage.getItem('currentUser');
+    const initialCurrentUser: User | undefined =
+      storedCurrentUser
+        ? JSON.parse(storedCurrentUser)
+        : undefined;
     // set state from context///
     userLogged?.setCurrentUser(initialCurrentUser);
   };
   ////
   const onSubmit = async (data: FormData) => {
     setLoading(true);
-    try{
-
-     const resp = await AuthService.login({
+    data.mail = data.mail.toLocaleLowerCase();
+    try {
+      const resp = await AuthService.login({
         email: data.mail,
         password: data.password,
       });
-    
-      if(resp.hasError){
-        toast.error("Email o contrasena incorrecto");
+
+      if (resp.hasError) {
+        toast.error('Email o contrasena incorrecto');
       } else {
         shoppingCartService.setEmpyArray(); //init cart on empty array
         await getUser(); // this func set the user on context
-        toast.success("Usuario ingresó correctamente");
-
+        toast.success('Usuario ingresó correctamente');
       }
-    } catch(err : any) {
+    } catch (err: any) {
       toast.error('que hace pa');
-      console.log('error', err)
+      console.log('error', err);
     }
     setLoading(false);
   };
   ////
   const resetErrorsMessagges = () => {
-    setErrorMessage("");
+    setErrorMessage('');
   };
   const confirmRecoverPassword = async (
-    _event: React.MouseEvent<HTMLElement>
+    _event: React.MouseEvent<HTMLElement>,
   ) => {
     if (
       confirm(
-        `Si, necesito que me envien un código a mi correo ${email} para recuperar mi contraseña`
+        `Si, necesito que me envien un código a mi correo ${email} para recuperar mi contraseña`,
       )
     ) {
       setLoading(true);
-      setError("password", {});
+      setError('password', {});
       const isEmailValid = await AuthService.passRecover({
         email: email,
       });
       /// show message when e mail is wrong
       if (isEmailValid.data === null) {
         setLoading(false);
-        setErrorMessage("Ingrese un email válido");
+        setErrorMessage('Ingrese un email válido');
         return;
       }
       /// render the forgotpassword component
@@ -120,11 +127,11 @@ const Login = () => {
   };
   /////
 
-
   return !isUserForgotPass ? (
-
-    
-    <div className="background-login" style={{ pointerEvents: loading ? "none" : "auto" }}>
+    <div
+      className="background-login"
+      style={{ pointerEvents: loading ? 'none' : 'auto' }}
+    >
       <div className="wrapper">
         <Link to="/">
           <img
@@ -138,37 +145,50 @@ const Login = () => {
           <div className="logintitle">Iniciar sesión</div>
         </div>
         <div className="wrapperlogin">
-          <div >
+          <div>
             <form
               id="email-form"
               name="email-form"
               data-name="Email Form"
               method="get"
-              style={{display: "flex", flexDirection: "column", gap: 8}}
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 8,
+              }}
               onSubmit={handleSubmit(onSubmit)}
               onFocus={resetErrorsMessagges}
-              
             >
-              <label htmlFor="name" className="txtlabel" style={{color:'black'}}>
+              <label
+                htmlFor="name"
+                className="txtlabel"
+                style={{ color: 'black' }}
+              >
                 Email
               </label>
               <input
                 className="inputlogin w-input"
                 maxLength={256}
-                {...register("mail")}
+                {...register('mail')}
                 onChange={(e) => setEmail(e.target.value)}
               />
-              <p style={{ color: "red" }}></p>
-              <label htmlFor="email" className="txtlabel" style={{color:'black'}}>
+              <p style={{ color: 'red' }}></p>
+              <label
+                htmlFor="email"
+                className="txtlabel"
+                style={{ color: 'black' }}
+              >
                 Contraseña
               </label>
               <input
                 type="password"
                 className="inputlogin w-input"
                 maxLength={256}
-                {...register("password")}
+                {...register('password')}
               />
-              <p style={{ color: "red" }}>{errors.password?.message}</p>
+              <p style={{ color: 'red' }}>
+                {errors.password?.message}
+              </p>
               {/* <div
                 style={{
                   margin: "10px",
@@ -191,40 +211,46 @@ const Login = () => {
               </div> */}
               <div
                 style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  width: "100%",
+                  display: 'flex',
+                  justifyContent: 'center',
+                  width: '100%',
                 }}
               >
-                <button className="buttonlogin"
+                <button
+                  className="buttonlogin"
                   style={{
-                    backgroundColor: "#2c3b5f",
-                    borderRadius: "13px",
-                    color: "white",
-                    padding: "10px",
-                    marginBottom: "22px",
-                    fontSize: "16px",
+                    backgroundColor: '#2c3b5f',
+                    borderRadius: '13px',
+                    color: 'white',
+                    padding: '10px',
+                    marginBottom: '22px',
+                    fontSize: '16px',
                     fontWeight: 200,
                     width: '120px',
-                    
                   }}
                   type="submit"
                 >
                   {loading ? (
-                  <CircularProgress size="1rem" />
-                ) : (
-                  'Ingresar')}
+                    <CircularProgress size="1rem" />
+                  ) : (
+                    'Ingresar'
+                  )}
                 </button>
               </div>
             </form>
           </div>
           <div
-            style={{ display: "grid", placeItems: "center", marginTop: "5px", gap: "5px"}}
+            style={{
+              display: 'grid',
+              placeItems: 'center',
+              marginTop: '5px',
+              gap: '5px',
+            }}
           >
             <button
               style={{
-                background: "transparent",
-                fontSize: "16px"
+                background: 'transparent',
+                fontSize: '16px',
               }}
               onClick={confirmRecoverPassword}
             >
@@ -233,9 +259,9 @@ const Login = () => {
             <Link
               to="/shortSignUp"
               style={{
-                textDecoration: "none",
-                color: "inherit",
-                fontSize: "16px"
+                textDecoration: 'none',
+                color: 'inherit',
+                fontSize: '16px',
               }}
             >
               Crear una cuenta
